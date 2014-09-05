7194db8b64781a53a50f038b861092e55465a9a9
edwardsCam <cedwards3054@gmail.com>
2014-09-05 13:19:16 -0600

    color

--- main.cpp
+++ main.cpp
@@ -3,26 +3,29 @@
 #include "SFML\Graphics.hpp"
 #include "SFML\Window.hpp"
 #include <math.h>
+#include <vector>
 
-#define WIDTH 1200
-#define HEIGHT 600
+#define WIDTH 1200.0
+#define HEIGHT 600.0
 #define phi ((sqrt(5) + 1)/2)
 #define mlen (min(WIDTH, HEIGHT) - 100.0)
-#define max_level 20
+#define max_level 10
 
 sf::RenderWindow window;
+std::vector<sf::RectangleShape *> boxes;
 
 void makeSquare(float x, float y, float len) {
-	sf::RectangleShape box;
-	box.setPosition(x, y);
+	sf::RectangleShape * box = new sf::RectangleShape();
 	sf::Vector2f s(len, len);
-	box.setSize(s);
-	window.draw(box);
+	box->setSize(s);
+	box->setPosition(x, y);
+	window.draw(*box);
 	window.display();
+
+	boxes.push_back(box);
 }
 
 void draw(int l, float x, float y) {
-
 	if (l > max_level)
 		return;
 
@@ -37,11 +40,56 @@ void draw(int l, float x, float y) {
 	draw(++l, x+len, nexty);
 }
 
+bool inBounds() {
+	sf::Vector2i mouse = sf::Mouse::getPosition(window);
+	return (mouse.x > 20 && mouse.y > 20 && mouse.x < WIDTH - 20 && mouse.y < HEIGHT - 20);
+}
+
+bool isHoveringOverBox(sf::RectangleShape * b) {
+	sf::Vector2i mouse = sf::Mouse::getPosition(window);
+	sf::Vector2f p = b->getPosition();
+	return (mouse.x >= p.x && mouse.x <= p.x + b->getSize().x);
+}
+
+void colorBoxes(sf::RectangleShape * curr) {
+	bool hit = false;
+	int s = boxes.size();
+	int n = 0;
+	int m;
+	for (int i = 0; i < s; i++) {
+		if (boxes[i] == curr) {
+			hit = true;
+			m = s - i;
+		}
+		if (hit) {
+			int c = 255 - (n * 256 / m);
+			boxes[i]->setFillColor(sf::Color(c, c, c));
+			n++;
+		} else {
+			boxes[i]->setFillColor(sf::Color::White);
+		}
+		window.draw(*boxes[i]);
+	}
+	window.display();
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	window.create(sf::VideoMode(WIDTH, HEIGHT), "Fibonacci Tower");
 	window.clear(sf::Color::Cyan);
 	window.display();
 	draw(1, 20.0, 20.0);
 
-	while (true);
+	sf::RectangleShape * curr = 0;
+
+	while (window.isOpen()) {
+		if (inBounds()) {
+			for (int i = 0; i < boxes.size(); i++) {
+				if (isHoveringOverBox(boxes[i])) {
+					curr = boxes[i];
+				}
+			}
+		} else curr = 0;
+
+		colorBoxes(curr);
+	}
 }
